051a2b7f196f68c08815b9c24a3ee419562ce88d
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2026-02-23 03:07:06 +0100

    haha

--- src/Controller/InterventionController.php
+++ src/Controller/InterventionController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/intervention')]
 #[IsGranted('ROLE_MEDECIN')]
